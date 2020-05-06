ae3d17fb0da63bb55bdf97520110a4386495997c
hyjj097 <hyjj097@naver.com>
2020-05-06 12:21:11 +0900

    initial commit

--- app/src/main/java/com/android/developer/codegalleryapp/MainActivity.java
+++ app/src/main/java/com/android/developer/codegalleryapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.android.developer.codegalleryapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startbutton = (Button) findViewById(R.id.startbtn);
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "시작 화면으로 이동합니다", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent( MainActivity.this,MainScreen.class);
+                    startActivity(intent);
+                }
+        });
     }
 }
+
---
+++ app/src/main/java/com/android/developer/codegalleryapp/MainScreen.java
@@ -0,0 +1,20 @@
+package com.android.developer.codegalleryapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+
+
+    }
+
