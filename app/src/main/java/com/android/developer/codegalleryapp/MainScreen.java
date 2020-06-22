915c1fb97aa38181552860d1b9e59c3be1f51971
hyjj097 <hyjj097@naver.com>
2020-06-22 17:51:29 +0900

    initial commit

--- app/src/main/java/com/android/developer/codegalleryapp/MainScreen.java
+++ app/src/main/java/com/android/developer/codegalleryapp/MainScreen.java
@@ -1,21 +1,64 @@
 package com.android.developer.codegalleryapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
-
+import java.io.InputStream;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainScreen extends AppCompatActivity {
 
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
 
+
+        imageview = (ImageView)findViewById(R.id.imageView);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType (android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult (intent, GET_GALLERY_IMAGE);
+            }
+        });
     }
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+
+        }
 
 
     }
+}
+
+
+
+
+
+
 
